f5c8fdb6db774fc78d802bb2028d94d5a560b47f
Viktoriia Sliusarenko <63744648+ViktoriiaSliu@users.noreply.github.com>
2025-07-28 15:55:03 +0300

    .

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,22 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 import mochaPlugin from "eslint-plugin-mocha";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   globalIgnores([".node_modules/", "reports/", "eslint.config.js", "mocha-runner.js"]),
   mochaPlugin.configs.recommended,
+  eslintConfigPrettier,
   { files: ["**/*.{js,mjs,cjs}"], 
     plugins: { js }, 
     extends: ["js/recommended"], 
     languageOptions: { globals: globals.browser },
     rules: {
-      semi: ['error', 'always'],
-      quotes: ['error', 'single', { avoidEscape: true }],
       'no-unused-vars': ['warn'],
       'no-console': 'off',
       'prefer-const': 'warn',
+      'no-unexpected-multiline': 'off',
+      'no-unused-expressions': 'off',
     },
   },
 ]);
